fc74feeaea1e9535022355d72ba64c53d14e2c19
boflaherty3 <brendenoflaherty@gmail.com>
2025-01-20 12:42:36 -0500

    updating base to include /

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://infinite-limit.github.io",
-  base: 'marketing-site',
+  base: '/marketing-site',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
